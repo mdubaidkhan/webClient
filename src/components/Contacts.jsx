d43cea6fc65ab59b230e9e19da7b5de76d857065
Mohammed Ubaid Khan <56798024+mdubaidkhan@users.noreply.github.com>
2022-06-10 21:14:16 +0530

    Update Contacts.jsx

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -27,29 +27,7 @@ export default function Contacts({ contacts, changeChat }) {
             <img src={Logo} alt="logo" />
             <h3>snappy</h3>
           </div>
-          <div className="contacts">
-            {contacts.map((contact, index) => {
-              return (
-                <div
-                  key={contact._id}
-                  className={`contact ${
-                    index === currentSelected ? "selected" : ""
-                  }`}
-                  onClick={() => changeCurrentChat(index, contact)}
-                >
-                  <div className="avatar">
-                    <img
-                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
-                      alt=""
-                    />
-                  </div>
-                  <div className="username">
-                    <h3>{contact.username}</h3>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+         
           <div className="current-user">
             <div className="avatar">
               <img
